d782e2005901ab04bc0abd29d69a61a07675c3f7
lmoush <zaghlouleabdelhak@gmail.com>
2023-05-24 17:02:29 -0700

    abdelhak zarhloul

---
+++ bidm.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * pnt_iid - print ID function
+ * Credit Taha & Lmouch
+ */
+void pnt_iid(void)
+{
+char pid_str[100];
+int pid_len;
+
+pid_len = sprintf(pid_str, "PID: %d\n", getpid());
+write(STDOUT_FILENO, pid_str, pid_len);
+}
+/**
+ * pnt_iiid - print Prent ID function
+ * Credit Taha & Lmouch
+ */
+void pnt_iiid(void)
+{
+char ppid_str[100];
+int ppid_len;
+
+ppid_len = sprintf(ppid_str, "PPID: %d\n", getppid());
+write(STDOUT_FILENO, ppid_str, ppid_len);
+}
---
+++ pmt.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * thom_prompt - prompt function
+ * Credit Taha & Lmouch
+ */
+void thom_prompt(void)
+{
+write(STDOUT_FILENO, "#taha$ ", 7);
+}
